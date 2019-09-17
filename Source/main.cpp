dafee99d64b2989cca56f2bc4e652aaa05694a43
Hyukin Kown <901po3@gmail.com>
2019-09-17 14:14:23 -0400

    IsOnlyInstance_1
    working on

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,4 +1,27 @@
 #include<iostream>
+#include<Windows.h>
+#include<direct.h>
+
+bool IsOnlyInstance(LPCTSTR gameTitle)
+{
+	HANDLE handle = CreateMutex(NULL, TRUE, gameTitle);
+
+	if (GetLastError() != ERROR_SUCCESS)
+	{
+		HWND hWnd = FindWindow(gameTitle, NULL);
+
+		if (hWnd) 
+		{
+			// An instance of your game is already running.
+			ShowWindow(hWnd, SW_SHOWNORMAL);
+			SetFocus(hWnd);
+			SetForegroundWindow(hWnd);
+			SetActiveWindow(hWnd);
+			return false;
+		}
+	}
+	return true;
+}
 
 int main()
 {
