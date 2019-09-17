674514ba3c96d7900e24413b8f367a1dd5317885
Heaseo Chung <look0713@hotmail.com>
2019-09-17 13:10:30 -0400

    Upload check storage

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
+#include <window.h>
+#include <direct.h>
+
+bool CheckStorage(const DWORDLONG diskSpaceNeeded)
+{
+    int const drive = _getdrive();
+    struct _diskfree_t diskFree;
+    _getDiskfree(drive, &diskFree);
+    
+    unsigned __int64 const neededCluster;
+    neededCluster = diskSpaceNeeded / (diskFree.sectors_per_cluster * diskFree.bytes_per_sector);
+    
+    if(diskFree.avail_clusters < neededCluster)
+    {
+        GCC_ERROR("CheckStorage Failure: Not enough physical storage.");
+        return false;
+    }
+    return true;
+}
 
 int main()
 {
+    CheckStorage(300);
 	return 0;
 }
