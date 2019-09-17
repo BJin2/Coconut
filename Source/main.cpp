6f66d9739011e5482dbf48089cfbcc257f6ff9d3
Heaseo Chung <look0713@hotmail.com>
2019-09-17 14:25:14 -0400

    Feature done check cpu speed

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,4 +1,22 @@
 #include<iostream>
+#include<Windows.h>
+#include<direct.h>
+
+DWORD ReadCpuSpeed()
+{
+    DWORD BufSize = sizeof(DWORD);
+    DWORD dwMHz =0;
+    DWORD type = REG_DWORD;
+    HKEY hKey;
+    
+    long lError = RegOpenKeyEx(HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", 0, KEY_READ, &hKey);
+    
+    if(lError == ERROR_SUCCESS)
+    {   // query the key:
+        RegQueryValueEx(hKey, "MHz", NULL, &type, (LPBYTE) &dwMHz, &BufSize);
+    }
+    return dwMHz;
+}
 
 int main()
 {
