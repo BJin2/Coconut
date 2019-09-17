e069e55cfa20b7ba0500b865d5dcb5b6377d5732
HeaseoChung <heaseochung@gmail.com>
2019-09-17 14:53:49 -0400

    Work_2
    d

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,26 +4,63 @@
 
 bool IsOnlyInstance(LPCTSTR gameTitle)
 {
-	HANDLE handle = CreateMutex(NULL, TRUE, gameTitle);
+	HANDLE handle = OpenMutex(MUTEX_ALL_ACCESS, 0, gameTitle);
 
+	if (!handle) // Mutex doesn't exist. This is the first instance so create the mutex.
+	{
+		handle = CreateMutex(NULL, TRUE, gameTitle);
+	}
 	if (GetLastError() != ERROR_SUCCESS)
 	{
 		HWND hWnd = FindWindow(gameTitle, NULL);
 
-		if (hWnd) 
+		if (hWnd)
 		{
 			// An instance of your game is already running.
 			ShowWindow(hWnd, SW_SHOWNORMAL);
-			SetFocus(hWnd);
+			//SetFocus(hWnd);
 			SetForegroundWindow(hWnd);
-			SetActiveWindow(hWnd);
+			//SetActiveWindow(hWnd);
 			return false;
 		}
 	}
+	//else// The mutex exists so this is the second instance so return.
+	//{
+	//	// Find the window of First Instance
+	//	HWND hWnd = FindWindow(gameTitle, NULL);
+	//	if (hWnd)
+	//	{
+	//		ShowWindow(hWnd, SW_SHOWNORMAL);
+	//		SetForegroundWindow(hWnd);
+
+	//		return false;
+	//	}
+	//}
+
 	return true;
+
+
+	//if (GetLastError() != ERROR_SUCCESS)
+	//{
+	//	HWND hWnd = FindWindow(gameTitle, NULL);
+
+	//	if (hWnd) 
+	//	{
+	//		// An instance of your game is already running.
+	//		ShowWindow(hWnd, SW_SHOWNORMAL);
+	//		SetFocus(hWnd);
+	//		SetForegroundWindow(hWnd);
+	//		SetActiveWindow(hWnd);
+	//		return false;
+	//	}
+	//}
+	//return true;
 }
 
 int main()
 {
+	std::cout<< IsOnlyInstance("CoconutEngine-x64-Release") << std::endl;
+	
+	system("pause");
 	return 0;
 }
