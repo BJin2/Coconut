4ca29d79b73f6b1365afa8885790eabe22c9d6ac
Hyukin Kown <901po3@gmail.com>
2019-09-17 21:59:13 -0400

    Check it working fine
    It's working

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,59 +2,21 @@
 #include<Windows.h>
 #include<direct.h>
 
-bool IsOnlyInstance(LPCTSTR gameTitle)
-{
-	HANDLE handle = OpenMutex(MUTEX_ALL_ACCESS, 0, gameTitle);
+bool IsOnlyInstance(LPCTSTR gameTitle) {
 
-	if (!handle) // Mutex doesn't exist. This is the first instance so create the mutex.
-	{
-		handle = CreateMutex(NULL, TRUE, gameTitle);
+	HANDLE handle = CreateMutex(NULL, TRUE, gameTitle);
+	if (GetLastError() == ERROR_ALREADY_EXISTS) {
+		//HWND hWnd = FindWindow(gameTitle, NULL);
+		//if (hWnd != NULL) {
+		//	// An instance of your game is already running.
+		//	ShowWindow(hWnd, SW_SHOWNORMAL);
+		//	SetFocus(hWnd);
+		//	SetForegroundWindow(hWnd);
+		//	SetActiveWindow(hWnd);
+		//}
+		return false;
 	}
-	if (GetLastError() != ERROR_SUCCESS)
-	{
-		HWND hWnd = FindWindow(gameTitle, NULL);
-
-		if (hWnd)
-		{
-			// An instance of your game is already running.
-			ShowWindow(hWnd, SW_SHOWNORMAL);
-			//SetFocus(hWnd);
-			SetForegroundWindow(hWnd);
-			//SetActiveWindow(hWnd);
-			return false;
-		}
-	}
-	//else// The mutex exists so this is the second instance so return.
-	//{
-	//	// Find the window of First Instance
-	//	HWND hWnd = FindWindow(gameTitle, NULL);
-	//	if (hWnd)
-	//	{
-	//		ShowWindow(hWnd, SW_SHOWNORMAL);
-	//		SetForegroundWindow(hWnd);
-
-	//		return false;
-	//	}
-	//}
-
 	return true;
-
-
-	//if (GetLastError() != ERROR_SUCCESS)
-	//{
-	//	HWND hWnd = FindWindow(gameTitle, NULL);
-
-	//	if (hWnd) 
-	//	{
-	//		// An instance of your game is already running.
-	//		ShowWindow(hWnd, SW_SHOWNORMAL);
-	//		SetFocus(hWnd);
-	//		SetForegroundWindow(hWnd);
-	//		SetActiveWindow(hWnd);
-	//		return false;
-	//	}
-	//}
-	//return true;
 }
 
 int main()
