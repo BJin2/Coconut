7bf35dfb03852adc6954edc0d55fe99595412994
Jin <bomjan@naver.com>
2020-01-12 03:04:30 -0500

    Implementing transform component

--- Source/Actors/Components/Transform.cpp
+++ Source/Actors/Components/Transform.cpp
@@ -1,1 +1,185 @@
 #include "Transform.h"
+#include <DirectXPackedVector.h>
+
+using namespace DirectX;
+
+//XMVECTOR T = XMLoadFloat3(&float3);
+//XMStoreFloat3(&float3)
+void Transform::VStart()
+{
+}
+
+void Transform::VUpdate(float dt)
+{
+}
+
+#pragma region Getters
+#pragma region Global
+float3 Transform::GetPosition()
+{
+	return m_position;
+}
+float3 Transform::GetScale()
+{
+	return m_scale;
+}
+float4 Transform::GetOrientation()
+{
+	return m_orientation;
+}
+float3 Transform::GetRotation()
+{
+	//TODO implement vector -> quaternion
+	return float3();
+}
+matrix4 Transform::GetTransform()
+{
+	return m_transformMatrix;
+}
+#pragma endregion
+#pragma region Local
+float3 Transform::GetLocalPosition()
+{
+	return m_localPosition;
+}
+float3 Transform::GetLocalScale()
+{
+	return m_localScale;
+}
+float4 Transform::GetLocalOrientation()
+{
+	return m_localOrientation;
+}
+float3 Transform::GetLocalRotation()
+{
+	//TODO implement vector -> quaternion
+	return float3();
+}
+matrix4 Transform::GetLocalTransform()
+{
+	return m_localTransformMatrix;
+}
+#pragma endregion
+#pragma endregion
+
+#pragma region Setters
+#pragma region Global
+void Transform::SetPosition(const float3& _position)
+{
+	m_position = _position;
+	//TODO update local position
+}
+void Transform::SetPosition(float _x, float _y, float _z)
+{
+	SetPosition(float3(_x, _y, _z));
+}
+void Transform::SetPosition(const xmvector& _position)
+{
+	XMStoreFloat3(&m_position, _position);
+}
+void Transform::SetScale(const float3& _scale)
+{
+	m_scale = _scale;
+	//TODO update local scale
+}
+void Transform::SetScale(float _x, float _y, float _z)
+{
+	SetScale(float3(_x, _y, _z));
+}
+void Transform::SetScale(const xmvector& _scale)
+{
+	XMStoreFloat3(&m_scale, _scale);
+}
+void Transform::SetOrientation(const float4& _orientation)
+{
+	m_orientation = _orientation;
+	//TODO update local orientation
+}
+void Transform::SetOrientation(float _x, float _y, float _z, float _w)
+{
+	SetOrientation(float4(_x, _y, _z, _w));
+}
+void Transform::SetOrientation(const xmvector& _orientation)
+{
+	float4 temp;
+	XMStoreFloat4(&temp, _orientation);
+	SetOrientation(temp);
+}
+void Transform::SetRotation(const float3& _rotation)
+{
+	//TODO float3 -> quaternion
+}
+void Transform::SetRotation(float _x, float _y, float _z)
+{
+	SetRotation(float3(_x, _y, _z));
+}
+void Transform::SetRotation(const xmvector& _rotation)
+{
+	float3 temp;
+	XMStoreFloat3(&temp, _rotation);
+	SetRotation(temp);
+}
+#pragma endregion
+#pragma region Local
+void Transform::SetLocalPosition(const float3& _position)
+{
+	m_localPosition = _position;
+	//update global position
+}
+void Transform::SetLocalPosition(float _x, float _y, float _z)
+{
+	SetLocalPosition(float3(_x, _y, _z));
+}
+void Transform::SetLocalPosition(const xmvector& _position)
+{
+	float3 temp;
+	XMStoreFloat3(&temp, _position);
+	SetLocalPosition(temp);
+}
+void Transform::SetLocalScale(const float3& _scale)
+{
+	m_localScale = _scale;
+	//TODO update global scale
+}
+void Transform::SetLocalScale(float _x, float _y, float _z)
+{
+	SetLocalScale(float3(_x, _y, _z));
+}
+void Transform::SetLocalScale(const xmvector& _scale)
+{
+	float3 temp;
+	XMStoreFloat3(&temp, _scale);
+	SetLocalScale(temp);
+}
+void Transform::SetLocalOrientation(const float4& _orientation)
+{
+	m_localOrientation = _orientation;
+	//update global orientation
+}
+void Transform::SetLocalOrientation(float _x, float _y, float _z, float _w)
+{
+	SetLocalOrientation(float4(_x, _y, _z, _w));
+}
+void Transform::SetLocalOrientation(const xmvector& _orientation)
+{
+	float4 temp;
+	XMStoreFloat4(&temp, _orientation);
+	SetLocalOrientation(temp);
+}
+void Transform::SetLocalRotation(const float3& _rotation)
+{
+	//TODO float3 to quaternion
+}
+void Transform::SetLocalRotation(float _x, float _y, float _z)
+{
+	SetLocalRotation(float3(_x, _y, _z));
+}
+void Transform::SetLocalRotation(const xmvector& _rotation)
+{
+	float3 temp;
+	XMStoreFloat3(&temp, _rotation);
+	SetLocalRotation(temp);
+}
+#pragma endregion
+#pragma endregion
+
--- Source/Actors/Components/Transform.h
+++ Source/Actors/Components/Transform.h
@@ -1,7 +1,86 @@
 #pragma once
+#include <DirectXMath.h>
 #include "ActorComponent.h"
+
+typedef DirectX::XMFLOAT3 float3;
+typedef DirectX::XMFLOAT4 float4;
+typedef DirectX::XMVECTOR xmvector;
+typedef DirectX::XMFLOAT4X4 matrix4;
+typedef DirectX::XMMATRIX xmmatrix;
 
 class Transform : public ActorComponent
 {
+private:
+	float3 m_position;
+	float3 m_scale;
+	float4 m_orientation;
+	
+	float3 m_localPosition;
+	float3 m_localScale;
+	float4 m_localOrientation;
+
+	matrix4 m_translationMatrix;
+	matrix4 m_scaleMatrix;
+	matrix4 m_rotationMatrix;
+
+	matrix4 m_transformMatrix;
+	matrix4 m_localTransformMatrix;
+public:
+	virtual void VStart() override;
+	virtual void VUpdate(float dt) override;
+
+#pragma region Getters
+#pragma region Global
+	float3 GetPosition();
+	float3 GetScale();
+	float4 GetOrientation();
+	float3 GetRotation();
+	matrix4 GetTransform();
+#pragma endregion
+#pragma region Local
+	float3 GetLocalPosition();
+	float3 GetLocalScale();
+	float4 GetLocalOrientation();
+	float3 GetLocalRotation();
+	matrix4 GetLocalTransform();
+#pragma endregion
+#pragma endregion
+
+#pragma region Setters
+#pragma region Global
+	void SetPosition(const float3& _position);
+	void SetPosition(float _x, float _y, float _z);
+	void SetPosition(const xmvector& _position);
+
+	void SetScale(const float3& _scale);
+	void SetScale(float _x, float _y, float _z);
+	void SetScale(const xmvector& _scale);
+
+	void SetOrientation(const float4& _orientation);
+	void SetOrientation(float _x, float _y, float _z, float _w);
+	void SetOrientation(const xmvector& _orientation);
+
+	void SetRotation(const float3& _rotation);
+	void SetRotation(float _x, float _y, float _z);
+	void SetRotation(const xmvector& _rotation);
+#pragma endregion
+#pragma region Local
+	void SetLocalPosition(const float3& _position);
+	void SetLocalPosition(float _x, float _y, float _z);
+	void SetLocalPosition(const xmvector& _position);
+
+	void SetLocalScale(const float3& _scale);
+	void SetLocalScale(float _x, float _y, float _z);
+	void SetLocalScale(const xmvector& _scale);
+
+	void SetLocalOrientation(const float4& _orientation);
+	void SetLocalOrientation(float _x, float _y, float _z, float _w);
+	void SetLocalOrientation(const xmvector& _orientation);
+
+	void SetLocalRotation(const float3& _rotation);
+	void SetLocalRotation(float _x, float _y, float _z);
+	void SetLocalRotation(const xmvector& _rotation);
+#pragma endregion
+#pragma endregion
 
 };
