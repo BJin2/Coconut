a4a9ec96c7052accc72bdb1b24801c56ddd082e0
Jin <bomjan@naver.com>
2020-04-19 19:24:38 -0400

    Fixed xml compiler error

--- Source/Engines/Game.cpp
+++ Source/Engines/Game.cpp
@@ -8,7 +8,7 @@
 #include "AudioEngine.hpp"
 #include "ScriptManager.hpp"
 #include "../CoconutEngine/CoconutEngine/resource.h"
-#include "XMLCompiler.h"
+#include "../Engines/XMLCompiler.h"
 
 //Event example
 #include "Event/EventManager.h"
@@ -20,7 +20,7 @@ Game::GameState Game::gameState = Game::GameState::Uninitialized;
 Scene* Game::scene;
 Time* Game::time;
 EventMapper* Game::mapper;
-XMLComment* Game::xml;
+XMLCompiler* Game::xml;
 
 // The main window class name.  
 static TCHAR szWindowClass[] = _T("win32app");
